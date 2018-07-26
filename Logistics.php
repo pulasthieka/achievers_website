04675ee60164c7f52a855c407ef805bc0e5c27fc
PUlasthi Ekanayake <puasthieka@yahoo.com>
2018-07-27 01:02:52 +0530

    Updated Logistics page

--- Logistics.php
+++ Logistics.php
@@ -13,10 +13,12 @@
     <?php include_once("navbar.php") ?>
 <!--Navbar over -->
 	  
-	 <div class="container">
+	 <div id ="main-head" class="container">
 <div class="row">
 		  <div class="col-md-3">
 	<img class="card-img-top" src="images/Courses/LIT.jpg" width="80%">
+			  <img class="card-img-top" src="images/Courses/FIATA-logo-D61CFBC1B6-seeklogo.com.png" width="80%"><br><br>
+			  <img class="card-img-top" src="images/Courses/SLFFA_Logo1.jpg" width="80%">
 			   <div class="card">
 					  <div class="card-header"> Content</div>
                 <ul class="list-group list-group-flush">
@@ -29,24 +31,10 @@
 			
 	</div>
 	<div class="col-md-9">
-		<p>&nbsp;</p><div class=" card-body">
-		<p class="text-justify">Logistics is an ever-expanding industry with seamless opportunities both locally and internationally. Candidates can look forward to challenging career opportunities in industry-specific domains or those that cut across many industries.</p>
-									<p class="text-justify">In January 2012, the Academy of International Trade and Transport (AITT) the training arm of the Sri Lanka Logistics and Freight Forwarders&rsquo; Association (SLFFA) appointed Achievers Lanka Business School as their exclusive training partner to facilitate the conduct of the well renowned logistics and freight forwarding qualifications offered by AITT. Achievers Lanka Business School provides the training facilities and academic guidelines for AITT.</p>
-									<p class="text-justify">The programmes offered are designed to equip students with effective technical and managerial skills necessary to become efficient logisticians at the supervisory and managerial levels. In addition, the programmes aim to provide the essential knowledge and pre-requisites for the pursuit of further education in business or logistics management.</p>
-									<p class="text-justify">The field of logistics and supply chain management is diverse. The future of the industry is bright in ever expanding globalization of manufacturing and sourcing of goods and technologies, that will be taking place due to ever demanding costs of products by the markets. Sri Lanka is well position to plug into the growth in global and regional trade for the Indian sub-continent region, being next door to India one of the largest manufacturing economies and consumer market. The multi-disciplinary nature of Logistics and Supply Chain Management programmes today offered by universities, institutes and academies provides management graduates with an edge to compete successfully in many industries.</p>
-		<div id="read_more" class="collapse">	
-									<p class="text-justify">Candidates can look forward to challenging career opportunities in industry-specific domains or those that cut across many industries. These can fit many job descriptions such as logistics/transport/shipping manager, inventory control and materialist specialist, supply chain manager, warehousing manager, port/airport manager, trade pricing specialist, import/export manager, procurement manager, channel sales manager and customer logistics analyst.</p>
-									<p class="text-justify">There are two paths-ways, one can get an entry to this industry, by following an academic path (Graduate programme in Logistics and Supply Chain Management, MBA in International Trade and Logistics) and join the industry and which still needs to undergo an apprentice period to learn the skills which is highly required to perform in real life situations to overcome the challenges and solutions that are demanded by the market.</p>
-									<p class="text-justify">The other is to join the industry and undergo an apprenticeship period of leaning the skills to perform services on an international environment and take-up professional courses as you start to gain practical exposure, like learning foundation aspects of the industry, followed by a certificate and diploma and higher diploma courses which will enhance your knowledge easily as you have already been exposed to the required level of skills on the practical side, the 2nd option one can say would in actual fact develop a much faster level of acquiring professional standards in one self with this combination of experience and skill to be further enhanced with an academic study path.</p>
-									<p class="text-justify">Both learning approaches can be considered fruitful and constructive pathways for new comers to this industry.</p>
-									<p class="text-justify">Today in the post war- era of Sri Lanka, with the building of several ports and airports infrastructure, the logistics industry will have several opportunities for employment of professionals which the country needs to have in-order to successfully managed these investments for the nation.</p>
-		</div>	
-		<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#read_more" aria-expanded="false" aria-controls="read_more">
-   Read More
-  </button><p>&nbsp;</p>
-		<h3 id="headingOne">Foundation Course on Multimodal Transport &amp; Logistics</h3>
-									<p class="text-justify">The Foundation course on Multimodal Transport &amp; Logistics is tailored to facilitate an entry into the freight forwarding and logistics industry and is ideal for school leavers wishing to join the industry.<br>
-									The course is conducted in association with the United Nations Economic and Social Commission for Asia and the Pacific (UNESCAP).<br>
+		
+		<img id="headingOne" class="img-fluid" src="images/Logistics1.png" alt="Foundation Course">
+		
+									<p class="text-justify">The Foundation course on Multimodal Transport &amp; Logistics is tailored to facilitate an entry into the freight forwarding and logistics industry and is ideal for school leavers wishing to join the industry.The course is conducted in association with the United Nations Economic and Social Commission for Asia and the Pacific (UNESCAP).<br>
 									The duration of the programme is 2 months and covers the basics that are essential for beginners joining the industry.</p>
 		<div id="read_more2" class="collapse">
 		
@@ -173,9 +161,9 @@
 		<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#read_more2" aria-expanded="false" aria-controls="read_more2">
    Detailed Syllabus
   </button><p>&nbsp;</p>
-		<h3 id="headingTwoOne">Certificate Course in Logistic Services, Freight Forwarding &amp; Multi Modal Transport</h3>
-									<p class="text-justify">Certificate Course in Logistic Services, Freight Forwarding &amp; Multi Modal Transport is designed for professionals with two years of industry experience.<br>
-									This course qualifies the company / student with the requirements stipulated by the Director of Merchant Shipping for the licensing of Freight Forwarders through the Gazette dated 2 August 2011.<br>
+		<img id="headingTwoOne" class="img-fluid" src="images/Logistics2.png" alt="Foundation Course">
+		
+									<p class="text-justify">Certificate Course in Logistic Services, Freight Forwarding &amp; Multi Modal Transport is designed for professionals with two years of industry experience.This course qualifies the company / student with the requirements stipulated by the Director of Merchant Shipping for the licensing of Freight Forwarders through the Gazette dated 2 August 2011.<br>
 									The duration of the programme is 4 months and covers key areas to enhance knowledge in logistics by focusing on the theoretical aspects.</p>
 	<div id="read_more3" class="collapse">							
 	<table class="table">
@@ -322,7 +310,7 @@
 	<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#read_more3" aria-expanded="false" aria-controls="read_more3">
   Detailed Syllabus
   </button><p>&nbsp;</p>
-		<h3 id="headingTwoTwo">FIATA International Diploma</h3>
+		<img id="headingTwoTwo" class="img-fluid" src="images/Logistics3.png">
 									<p class="text-justify">The FIATA International Diploma is a globally recognized Diploma designed for professionals in a managerial level position within the freight forwarding and logistics industry. The duration of the programme is 2 years.</p>
 									
 		<div id="read_more4" class="collapse">							
