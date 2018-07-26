4bee1b5a1ae1b7b5a72c235f7880567bcca5ac07
PUlasthi Ekanayake <puasthieka@yahoo.com>
2018-07-26 21:06:07 +0530

    Perfecting the carousel and course cards

--- Home.php
+++ Home.php
@@ -26,7 +26,7 @@
 	  <li data-target="#main-head" data-slide-to="7"></li>
 	  <li data-target="#main-head" data-slide-to="8"></li>
 	  <li data-target="#main-head" data-slide-to="9"></li>
-	  <li data-target="#main-head" data-slide-to="10"></li>
+	  
 	  
   </ul-->
 
@@ -60,9 +60,6 @@
       <img src="images/fastrack4.jpg" alt="Unilever" class="img-fluid">
     </div>
 	  <div class="carousel-item">
-      <img src="images/3.jpg" alt="fastrack" class="img-fluid">
-    </div>
-	  <div class="carousel-item">
       <img src="images/rotract2.jpg" alt="Rotract2" class="img-fluid">
     </div>
 	  <div class="carousel-item">
@@ -80,45 +77,45 @@
 	  </div>
     <div class="container">
 		 <div class="row">
-   <div class="col-md-3" id="CIMA">
-	   <div class="card">
+   <div class="col-md-3 d-flex" id="CIMA">
+	   <div class="card flex-fill">
                 <div class="card-body">
 				<div class="card-img-top"><a href="CIMA.php"><img src="images/Courses/cima.jpg" class="img-fluid"></a></div>
                   
                   <h6 class="card-subtitle mb-2 text-muted">Chartered Institute of Management Accountants</h6>
-                  <p class="card-text">CIMA is the world's largest professional body of management accountants </p>
+                  <p class="card-text mobile-hide">CIMA is the world's largest professional body of management accountants </p>
                   <a href="CIMA.php" class="card-link">Study CIMA</a>
 				<a href="https://www.cimaglobal.com/" class="card-link weblink">Visit&nbsp;Website</a>
                   </div>
     		</div>
 	</div>
-	<div class="col-md-3" id="ACCA">
+	<div class="col-md-3 " id="ACCA">
              <div class="card">
                 <div class="card-body">
                   <div class="card-img-top"><a href="ACCA.php"><img src="images/Courses/acca.png" class="img-fluid"></a></div>
                   <h6 class="card-subtitle mb-2 text-muted"> Association of Chartered Certified Accountants</h6>
-                  <p class="card-text">All over the world, employers seek out ACCA members to lead their business to sustainable success. </p>
+                  <p class="card-text mobile-hide">All over the world, employers seek out ACCA members to lead their business to sustainable success. </p>
                   <a href="ACCA.php" class="card-link">Study ACCA</a> 
 					<a href="http://www.accaglobal.com/gb/en.html" class="card-link weblink">Visit&nbsp;Website</a>
     		</div>
 		</div></div>
-	<div class="col-md-3" id="CISI">
-             <div class="card">
+	<div class="col-md-3 d-flex" id="CISI">
+             <div class="card flex-fill">
                 <div class="card-body">
                    <div class="card-img-top"><a href="CISI.php"><img src="images/Courses/cisi.png" class="img-fluid"></a></div>
                    <h6 class="card-subtitle mb-2 text-muted">Chartered Institute for Securities &amp; Investment</h6>
-                   <p class="card-text">The professional body of choice for the financial services sector.</p>
+                   <p class="mobile-hide card-text">The professional body of choice for the financial services sector.</p>
                    <a href="CISI.php" class="card-link">Study CISI</a>
                    <a href="https://www.cisi.org/cisiweb2" class="card-link weblink">Visit&nbsp;Website</a>
     		</div>
 		</div>
 			 </div>
-	<div class="col-md-3" id="Logistics">
+	<div class="col-md-3 d-flex" id="Logistics">
              <div class="card">
                 <div class="card-body">
                 <div class="card-img-top"><a href="Logistics.php"><img src="images/Courses/LIT.jpg" class="img-fluid"></a></div>
-                   <h6 class="card-subtitle mb-2 text-muted">LIT</h6>
-                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                   <h6 class="card-subtitle mb-2 text-muted">Logistics</h6>
+                   <p class="card-text mobile-hide">Study certified Logistics courses at Achievers</p>
                    <a href="Logistics.php" class="card-link">Study Logistics</a>
                    
                 </div>
